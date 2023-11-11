5da9bdf390454ab6a6a14f6b5e661de74acdbbab
KmnPetr <kmn.petrichenko@yandex.ru>
2023-11-11 14:10:53 +0300

    commit

---
+++ src/main/java/com/example/Mandelbrot/MandelbrotApplication.java
@@ -0,0 +1,17 @@
+package com.example.Mandelbrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class MandelbrotApplication {
+	public static void main(String[] args) {
+//		SpringApplication.run(MandelbrotApplication.class, args);
+
+		FractalExplorer displayExplorer = new FractalExplorer(100);
+		displayExplorer.createAndShowGUI();
+		displayExplorer.drawFractal();
+
+	}
+
+}
---
+++ src/test/java/com/example/Mandelbrot/MandelbrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mandelbrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandelbrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
